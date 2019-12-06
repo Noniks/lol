b5d96234286d864ae601ced5167cebc88f6acf6f
Noniks <57525128+Noniks@users.noreply.github.com>
2019-12-07 02:39:14 +0700

    Update 23.php

--- 23.php
+++ 23.php
@@ -1,6 +1,6 @@
 <?php
 date_default_timezone_set('Asia/Jakarta');
-include "function.php";
+include "ep.php";
 echo color("red","                                                                                         
                                                                                                     
                                                                                                     
