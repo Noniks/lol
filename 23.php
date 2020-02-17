4fe94aa21e16f06194fb6e197b852c19b634f281
Noniks <57525128+Noniks@users.noreply.github.com>
2020-02-17 12:02:03 +0700

    Update 23.php

--- 23.php
+++ 23.php
@@ -63,7 +63,7 @@ echo " ===========================\n";
         echo color("yellow",".");
         sleep(1);
         }
-        $code1 = request('/go-promotions/v1/promotions/enrollments', $token, '{"promo_code":"GOFOODSANTAI19"}');
+        $code1 = request('/go-promotions/v1/promotions/enrollments', $token, '{"promo_code":"G-GQT2MMS"}');
         $message = fetch_value($code1,'"message":"','"');
         if(strpos($code1, 'Promo kamu sudah bisa dipakai')){
         echo "\n".color("green","+] Message: ".$message);
